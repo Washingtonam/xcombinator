cf16d459af968b437848233f640531399f0a395c
Washington Amedu <washingtonamedu@gmail.com>
2026-03-21 10:15:37 +0100

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Login from "./pages/Login";
 
@@ -8,23 +8,43 @@ import VerifyBVN from "./pages/VerifyBVN";
 import Transactions from "./pages/Transactions";
 import Wallet from "./pages/Wallet";
 
+function Layout() {
+  return (
+    <div className="flex">
+      <Sidebar />
+
+      <div className="flex-1 p-10">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/verify-nin" element={<VerifyNIN />} />
+          <Route path="/verify-bvn" element={<VerifyBVN />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route path="/wallet" element={<Wallet />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+function AppRoutes() {
+  const location = useLocation();
+
+  // 👉 if login page, don't show sidebar layout
+  if (location.pathname === "/login") {
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    );
+  }
+
+  return <Layout />;
+}
+
 function App() {
   return (
     <BrowserRouter>
-      <div className="flex">
-        <Sidebar />
-
-        <div className="flex-1 p-10">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/verify-nin" element={<VerifyNIN />} />
-            <Route path="/verify-bvn" element={<VerifyBVN />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/wallet" element={<Wallet />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
